c543e3c0018bb5fe4644809a3610ba32c6558915
PeryCreep <polyakov0202@bk.ru>
2024-05-16 08:08:45 +0500

    services realization

--- src/main/java/domain/ports/in/service/IOfficeService.java
+++ src/main/java/domain/ports/in/service/IOfficeService.java
@@ -1,4 +1,19 @@
 package domain.ports.in.service;
 
 public interface IOfficeService {
+
+     /**
+     * Получение всех офисов хранящихся в сервисе.
+     * 
+     * @return список всех офисов
+     */
+    List<Workplace> getAllWorkplaces();
+
+    /**
+     * Получение информации о конкретном офисе.
+     * 
+     * @param uuid уникальный идентификатор офиса
+     * @return информация о рабочем месте
+     */
+    Workplace getWorkplaceByUuid(String uuid);
 }
--- src/main/java/domain/ports/in/service/IWarehouseService.java
+++ src/main/java/domain/ports/in/service/IWarehouseService.java
@@ -1,4 +1,19 @@
 package domain.ports.in.service;
 
 public interface IWarehouseService {
+
+    /**
+     * Получение всех складов хранящихся в сервисе.
+     * 
+     * @return список всех складов
+     */
+    List<Warehouse> getAllWarehouses();
+
+    /**
+     * Получение информации о конкретном складе.
+     * 
+     * @param uuid уникальный идентификатор склада
+     * @return информация о складе
+     */
+    Warehouse getWarehouseByUuid(String uuid);
 }
